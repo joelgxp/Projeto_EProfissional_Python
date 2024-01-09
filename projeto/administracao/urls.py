ac8a988e06ae9b74093e7ee1e62acad0c0497a37
Joel <joelvieirasouza@gmail.com>
2024-01-09 17:31:10 -0300

    Update urls.py

--- projeto/administracao/urls.py
+++ projeto/administracao/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.urls.base import reverse_lazy
 from .views import servicos_views, usuarios_views
 from django.contrib.auth import views as auth_views
 
@@ -14,6 +15,10 @@ urlpatterns = [
     
     path('autenticacao/login', auth_views.LoginView.as_view(), name='logar-usuario'),
     path('autenticacao/logout', auth_views.LogoutView.as_view(), name='sair-usuario'),
-    path('alterar_senha', auth_views.PasswordChangeView.as_view(), name='alterar-senha'),
+    path('alterar_senha', auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('listar-servicos')), name='alterar-senha'),
+    path('resetar_senha', auth_views.PasswordResetView.as_view(), name='resetar-senha'),
+    path('resetar_senha/sucesso', auth_views.PasswordResetDoneView.as_view(), name='resetar-senha-sucesso'),
+    path('resetar_senha/<str:uidb64>/<str:token>', auth_views.PasswordResetConfirmView.as_view(), name='resetar-senha-confirmar'),
+    path('resetar_senha/feito', auth_views.PasswordResetDoneView.as_view(), name='resetar-senha-completo'),
     
 ]
