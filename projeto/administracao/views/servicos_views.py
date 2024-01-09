1865ade47a94fcd1700f0d2d9b17fbcf3e9dda9f
Joel <joelvieirasouza@gmail.com>
2024-01-09 17:29:41 -0300

    Update servicos_views.py

--- projeto/administracao/views/servicos_views.py
+++ projeto/administracao/views/servicos_views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 from ..forms.servico_forms import ServicoForm
 from ..models import Servico
 
+@login_required
 def cadastrar_servico(request):
     if request.method == 'POST':
         form_servico = ServicoForm(request.POST)
@@ -11,11 +13,12 @@ def cadastrar_servico(request):
     else:
         form_servico = ServicoForm()
     return render(request, 'servicos/form_servicos.html', {"form_servico": form_servico})
-
+@login_required
 def listar_servicos(request):
     servicos = Servico.objects.all()
     return render(request, 'servicos/lista_servicos.html', {"servicos": servicos})
 
+@login_required
 def editar_servico(request, id):
     servico = Servico.objects.get(id=id)
     form_servico = ServicoForm(request.POST or None, instance=servico)   
@@ -24,6 +27,7 @@ def editar_servico(request, id):
         return redirect('listar-servicos')
     return render(request, 'servicos/form_servicos.html', {"form_servico": form_servico})
 
+@login_required
 def deletar_servico(request, id):
     servico = Servico.objects.get(id=id)
     form_servico = ServicoForm(request.POST or None, instance=servico)
