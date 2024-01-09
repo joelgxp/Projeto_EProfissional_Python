a13e985d1683ef0f35f5248e78a381104c3ce793
Joel <joelvieirasouza@gmail.com>
2024-01-09 17:30:46 -0300

    Update usuarios_views.py

--- projeto/administracao/views/usuarios_views.py
+++ projeto/administracao/views/usuarios_views.py
@@ -1,22 +1,26 @@
 from ..forms.usuario_form import CadastroUsuarioForm, EditarUsuarioForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def cadastrar_usuario(request):
     if request.method == "POST":
         form_usuario = CadastroUsuarioForm(request.POST)
         if form_usuario.is_valid():
             form_usuario.save()
-            return redirect('listar-usuarios')
+            return redirect('listar-usuario')
     else:
         form_usuario = CadastroUsuarioForm()
     return render(request, 'usuarios/form_usuario.html', {'form_usuario': form_usuario})
 
+@login_required
 def listar_usuarios(request):
     User = get_user_model()
     usuarios = User.objects.filter(is_superuser=True)
     return render(request, 'usuarios/lista_usuarios.html', {"usuarios": usuarios})
 
+@login_required
 def editar_usuario(request, id):
     User = get_user_model()
     usuario = User.objects.get(id=id)
@@ -24,4 +28,4 @@ def editar_usuario(request, id):
     if form_usuario.is_valid():
         form_usuario.save()
         return redirect('listar-usuarios')
-    return render(request, 'usuarios/form_usuarios.html', {"form_usuario": form_usuario})    
+    return render(request, 'usuarios/form_usuario.html', {"form_usuario": form_usuario})    
