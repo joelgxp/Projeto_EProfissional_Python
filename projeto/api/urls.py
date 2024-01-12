5e0551657d4ba2c2fad0c74d39bcd5808b5a2b8d
Joel <joelvieirasouza@gmail.com>
2024-01-12 19:04:56 -0300

    Update urls.py

--- projeto/api/urls.py
+++ projeto/api/urls.py
@@ -3,4 +3,5 @@ from .views import usuarios_view
 
 urlpatterns = [
     path('usuarios/localidades', usuarios_view.UsuariosView.as_view(), name='listar-usuarios-list'),
+    path('usuarios', usuarios_view.UsuariosView.as_view(), name='cadastrar-usuarios'),
 ]
