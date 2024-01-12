839e238dda4dd7cab0ddc42a81dcaeb712a20021
Joel <joelvieirasouza@gmail.com>
2024-01-12 19:05:09 -0300

    Update models.py

--- projeto/api/models.py
+++ projeto/api/models.py
@@ -22,8 +22,8 @@ def nome_arquivo_documento(instance, filename):
 
 class Usuario(AbstractUser):
     TIPO_USUARIO_CHOICES = (
-        ('P', 'Profissional'),
-        ('C', 'Cliente')
+        (1, 'Profissional'),
+        (2, 'Cliente')
     )
     
     nome_completo = models.CharField(max_length=200, null=True, blank=False)
@@ -31,10 +31,14 @@ class Usuario(AbstractUser):
     nascimento = models.DateField(null=True, blank=False)
     email = models.EmailField(null=False, blank=False, unique=True)
     celular = models.CharField(max_length=15, null=True, blank=False)
+    reputacao = models.FloatField(null=True, blank=False, default=5)
     tipo_usuario = models.CharField(max_length=1, choices=TIPO_USUARIO_CHOICES, null=True, blank=False)
+    chave_pix = models.CharField(max_length=255, null=True, blank=True)
     foto_de_perfil = models.ImageField(upload_to=nome_arquivo_foto, null=True, validators = [validate_image_file_extension, ])
     foto_documento = models.ImageField(upload_to=nome_arquivo_documento, null=True, validators=[validate_image_file_extension, ])    
-    ativo = models.BooleanField(default=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('nome_completo', 'cpf', 'celular', 'tipo_usuario', 'reputacao', 'chave_pix', 'foto_de_perfil', 'foto_documento', 'password', 'is_staff', 'is_superuser', 'is_active', 'tipo', 'data_nascimento', 'tipo', 'data_nascimento', 'tipo')
 
 class CidadesAtendimento(models.Model):
     codigo_ibge = models.IntegerField(null=False, blank=False)
