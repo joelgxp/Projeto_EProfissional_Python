5fbd320ea0cb2275832132b13c629757d73ac635
Joel <joelvieirasouza@gmail.com>
2024-01-14 12:22:13 -0300

    Update urls.py

--- projeto/api/urls.py
+++ projeto/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import usuarios_view
+from .views import usuarios_view, me_view
 
 urlpatterns = [
     path('usuarios/localidades', usuarios_view.UsuariosView.as_view(), name='listar-usuarios-list'),
     path('usuarios', usuarios_view.UsuariosView.as_view(), name='cadastrar-usuarios'),
+    path('me', me_view.MeView.as_view(), name='me-list'),
 ]
