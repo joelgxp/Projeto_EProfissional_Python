395b91edce52b4cb667b5532353d305b231f90cf
Joel <joelvieirasouza@gmail.com>
2024-01-14 21:04:36 -0300

    Update usuarios_view.py

--- projeto/api/views/usuarios_view.py
+++ projeto/api/views/usuarios_view.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from ..models import Usuario
-from ..serializers.usuarios_serializer import UsuariosSerializer
+from ..serializers import usuarios_serializer
 from rest_framework import status as status_code
 from ..services import cidades_atendimento_service
 from ..paginations import profissionais_localidade_pagination
@@ -13,6 +13,16 @@ class UsuariosView(APIView, profissionais_localidade_pagination.ProfissionaisLoc
         cep = self.request.query_params.get('cep', None)
         usuarios = cidades_atendimento_service.listar_prossionais_cidade(cep)
         usuarios_paginacao = self.paginate_queryset(usuarios, request)
-        serializer_usuarios = UsuariosSerializer(usuarios_paginacao, many=True, context={'request': request})
+        serializer_usuarios = usuarios_serializer.UsuariosSerializer(usuarios_paginacao, many=True, context={'request': request})
         return self.get_paginated_response(serializer_usuarios.data)
+    
+    def post(self, request, format=None):
+        serializer_usuario = usuarios_serializer.UsuariosSerializer(data=request.data, context={'request': request})
+        
+        if serializer_usuario.is_valid():
+            usuario_criado = serializer_usuario.save()
+            serializer_usuario = usuarios_serializer.UsuariosSerializer(usuario_criado)
+            return Response(serializer_usuario.data, status=status_code.HTTP_201_CREATED)
+        return Response(serializer_usuario.errors, status=status_code.HTTP_400_BAD_REQUEST)
+        
     
