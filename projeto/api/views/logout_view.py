796690805cd90e27d119832e15173323ee1630ff
Joel <joelvieirasouza@gmail.com>
2024-01-14 21:04:46 -0300

    Update logout_view.py

--- projeto/api/views/logout_view.py
+++ projeto/api/views/logout_view.py
@@ -2,11 +2,14 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from rest_framework import status as status_code
+from rest_framework import permissions
+
 
 class LogoutView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
     def post(self, request):
         try:
-            refresh_token = request.data["refresh_token"]
+            refresh_token = request.data['refresh']
             token = RefreshToken(refresh_token)
             token.blacklist()
             return Response(status=status_code.HTTP_205_RESET_CONTENT)
